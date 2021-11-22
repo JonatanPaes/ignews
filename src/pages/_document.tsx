d2be2f3feb19a283f4ad3b45c2d3486480d66f7e
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2021-11-22 20:48:01 -0300

    Title dinâmico por página

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,8 +10,6 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto:wght@400;700;900&display=swap"
             rel="stylesheet"
           />
-
-          <title>ig.news</title>
         </Head>
         <body>
           <Main />
