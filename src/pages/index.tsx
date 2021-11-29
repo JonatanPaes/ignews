53bed9e45c7ff119702b57d3aa7e46fc0049d11f
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2021-11-29 08:54:42 -0300

    Página Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,32 @@
 import Head from "next/head";
+import Image from "next/image";
+
+import avatar from "../../public/images/avatar.svg";
+
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Helow Juju</h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News abount the <span>React</span> world.
+          </h1>
+
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 moonth</span>
+          </p>
+        </section>
+
+        <Image src={avatar} alt="Girl coding" />
+      </main>
     </>
   );
 }
